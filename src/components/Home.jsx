0677949262ce83c07e464e88fdda4e1793f837f0
Stefano Casasola <ste.casasola@gmail.com>
2021-12-14 12:50:19 +0100

    Finishes the lecture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import { Carousel, Container, Row, Col } from 'react-bootstrap'
+import { Component } from 'react'
+import { Carousel, Container, Row, Col, ListGroup } from 'react-bootstrap'
 import dishes from '../data/menu.json'
 // dishes is the array with 5 objects inside of it (each one being a pasta dish)
 
@@ -7,31 +8,92 @@ import dishes from '../data/menu.json'
 // const newArray = myArray.map(element => element + 1)
 // newArray is now [2, 3, 4]
 
-const Home = () => (
-    <Container>
-        <Row className="justify-content-center">
-            <Col xs={12} md={6}>
-                <h2 className="my-2">Welcome to Strivestaurant</h2>
-                <Carousel>
-                    {
-                        dishes.map(dish => (
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src={dish.image}
-                                    alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>{dish.name}</h3>
-                                    <p>{dish.description}</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        ))
-                    }
-                </Carousel>
-            </Col>
-        </Row>
-    </Container>
-)
+// now the final boss: select a pasta and show its comments
+// 1) should find a way to select a pasta and remember it as time goes by
+// 2) we can now show its comments in the bottom of the page
+// 3) if we select a new pasta, the comment section should update
+
+// for remembering things, you need a state.
+// a state is a safe place that will keep pieces of info over time
+
+// for having a state, you need a Class Component
+// fortunately, converting a functional component into a class component is quite simple:
+
+// 1) import { Component } from 'react' <-- Component is the MAIN COMPONENT CLASS we want to extend from
+// 2) replace your function with a class
+// 3) the only mandatory method in a class is called render()
+// 4) put your JSX in the return statement of the render method
+
+class Home extends Component {
+
+    // use a class component when you need a state!
+    // our goal is keeping track of the selected pasta
+    state = {
+        // all the things you want to keep track of will be properties of your state object
+        selectedDish: null
+        // let's set the initial value of selectedDish
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row className="justify-content-center">
+                    <Col xs={12} md={6}>
+                        <h2 className="my-2">Welcome to Strivestaurant</h2>
+                        <Carousel>
+                            {
+                                dishes.map(dish => (
+                                    <Carousel.Item key={dish.id}>
+                                        {/* the key prop should be something unique */}
+                                        {/* and should be applied to the parent element you're */}
+                                        {/* returning out your map */}
+                                        <img
+                                            className="d-block w-100"
+                                            src={dish.image}
+                                            alt="First slide"
+                                            onClick={() =>
+                                                // here I should change the selectedDish in the state
+                                                // from initially null to a pasta
+                                                // but the state is read-only
+                                                // for changing it you have to use a method called setState()
+                                                this.setState(
+                                                    {
+                                                        selectedDish: dish
+                                                    }
+                                                )
+                                                // setState is a method and takes an object as the argument
+                                                // that object will be MERGED into the current state
+                                            }
+                                        />
+                                        <Carousel.Caption>
+                                            <h3>{dish.name}</h3>
+                                            <p>{dish.description}</p>
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
+                    </Col>
+                </Row>
+                {/* now the comments section! */}
+                <Row className="justify-content-center">
+                    <Col xs={12} md={6}>
+                        <ListGroup>
+                            {
+                                // the part before the && is just for not crash the application
+                                // because initially selectedDish is null, and we cannot map the comments
+                                // of something that is null!
+                                // I'm checking if selectedDish has a truthy value to execute the map()
+                                this.state.selectedDish && this.state.selectedDish.comments.map(c => (
+                                    <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+                                ))
+                            }
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
 
 export default Home
